09a463b46496e9d239e28e7cc3741dd5dc8859bd
sebeynott <seb@sebeynott.com>
2019-01-15 19:59:29 +0000

    Updates

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { PATHS } from '../../constants';
 
 import './styles.css';
+import h from '../../images/hero.jpg';
 
 export const Hero = () =>
   <div className="hero">
+    <div className="image">
+      <img
+        src={h}
+        alt="Seb Sowter"
+      />
+    </div>
     <div className="wrapper">
       <div className="grid">
-      
+        <div className="col-4">
+        </div>
+        <div className="col-8">
+          <div className="hero-intro">
+            <p>Developer, Creator</p>
+            <p>(mostly with JavaScript)</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
