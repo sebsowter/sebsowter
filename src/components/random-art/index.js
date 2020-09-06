4d26b9bc4934e42377b90c0e708058e0032ce701
sebsowter <seb@sebeynott.com>
2020-09-07 00:24:59 +0100

    TypeScript

--- src/components/random-art/index.js
+++ src/components/random-art/index.js
@@ -1,34 +1,34 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { PATHS } from '../../constants';
-import { toSlug } from '../../utils';
-import RandomAr from './art';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { PATHS } from "../../constants";
+import { toSlug } from "../../utils";
+//import RandomAr from './art';
 
 class RandomArt extends Component {
   constructor(props) {
     super(props);
-    
+
     this.element = null;
     this.setCanvasRef = (el) => {
       this.element = el;
     };
   }
 
-	componentDidMount() {
-    const art = new RandomAr(this.element);
-	}
+  componentDidMount() {
+    //const art = new RandomAr(this.element);
+  }
 
-	render() {
+  render() {
     const { category } = this.props;
 
-		return (
-			<div>
-        <p className="h4">{category || 'Post'}</p>
+    return (
+      <div>
+        <p className="h4">{category || "Post"}</p>
         <div className="art" ref={this.setCanvasRef}></div>
       </div>
-		);
-	}
+    );
+  }
 }
 
 export default RandomArt;
